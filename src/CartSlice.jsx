f9a7ecc047c07640544679fd1332b0988cbca716
MalakFathy14 <malakfathy041@gmail.com>
2024-09-11 22:09:15 +0300

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -5,14 +5,34 @@ export const CartSlice = createSlice({
   initialState: {
     items: [], // Initialize items as an empty array
   },
+
   reducers: {
     addItem: (state, action) => {
-    
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem){
+        existingItem.quantity++;
+      }
+      else{
+        state.items.push({name, image, cost, quantity:1});
+      }
     },
     removeItem: (state, action) => {
+      const {name, image, cost} = action.payload;
+      const Existingitem = state.items.find(item => item.name === name);
+      if(Existingitem){
+        state.items = state.items.filter(item => item.name !== name);
+      }
     },
-    updateQuantity: (state, action) => {
 
+    updateQuantity: (state, action) => {
+      const {name, quantity} = action.payload
+      // console.log(quantity)
+      const existingItem = state.items.find(item => item.name === name);
+      // console.log(existingItem.name)
+      if(existingItem){
+        existingItem.quantity = quantity
+      }
     
     },
   },
